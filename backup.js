3e17627c189bf0b7da9c6a433c724ff5daf5e364
Jose Bolivar <tu-correo@ejemplo.com>
2026-02-01 15:08:47 -0300

    commit

--- backup.js
+++ backup.js
@@ -22,7 +22,7 @@ function exportBackup() {
     [JSON.stringify(data, null, 2)],
     { type: "application/json" }
   );
-
+ 
   const a = document.createElement("a");
   a.href = URL.createObjectURL(blob);
   a.download = filename;
